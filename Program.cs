10fdc8c7cd1ef6169b02c42c26ff2e1a92bdf7d8
Kerem Durak <54421885+kerem97@users.noreply.github.com>
2021-05-10 04:24:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int toplam = 0, sayi;
+            int sayac = 0;
+            double ort = 0;
+            do
+            {
+                Console.Write("Sayı girin : ");
+                sayi = int.Parse(Console.ReadLine());
+                toplam += sayi;
+                sayac++;
+                ort = (double)toplam / sayac;
+
+
+            } while (sayi != 0);
+            Console.WriteLine("Toplam={0}", toplam);
+            Console.WriteLine("ORtalama={0}", ort);
+            Console.ReadKey();
+        }
+    }
+}
